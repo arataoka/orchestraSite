0c4a69607a21656c76b548b48041b319c7e98824
For Private <>
2020-07-06 19:15:41 +0900

    [feat] hamburger 追加実装

--- src/js/modules/hamburger.js
+++ src/js/modules/hamburger.js
@@ -3,13 +3,17 @@ class Hamburger {
     console.log('aaaaaa')
     this.humberger = document.querySelector('.js-hamburger')
     this.main = document.querySelector('.js-main')
+    this.menu = document.querySelector('.js-menu')
     this.humberger.addEventListener('click', this.toggleMenu.bind(this)) //ここでbindをする必要がある！
   }
   toggleMenu() {
     console.log(this.humberger)
     console.log(this.main)
     console.log('ccc')
+    this.menu.classList.toggle('is-open')
     this.main.classList.toggle('is-open')
+    this.main.style.position = 'fixed'
+    this.humberger.classList.toggle('is-open')
   }
 }
 
