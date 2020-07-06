fb8b777ddb69097ba2ee25b50618e92d741d4cbd
For Private <>
2020-07-06 15:06:54 +0900

    [TODO] swiper導入バグあり

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,15 @@
+const withCss = require('@zeit/next-css')
+module.exports = withCss()
+import Swiper from 'swiper'
+// import Swiper styles
+
 import '@scss/index'
+import 'swiper/swiper-bundle.css'
 import Hamburger from './modules/hamburger'
 
 window.addEventListener('DOMContentLoaded', () => {
   console.log('aaa')
   new Hamburger()
+  new Swiper('.swiper-container')
   console.log('bbb')
 })
